372d7ebe457b03aa4a3f537986d04798b0d0f035
Francisco Bejar <franbejar02@gmail.com>
2023-06-15 20:32:20 -0300

    Modificacion Text

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aca vas a aprender a hacer apps y llenarte de guita loco!!!!</Text>
+      <Text>Texto de prueba con el objetivo de ver que onda cuando jugamos con el limite de la pantalla</Text>
       <StatusBar style="auto" />
     </View>
   );
